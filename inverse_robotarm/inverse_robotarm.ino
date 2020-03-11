5da134f020eb1ac2ad67828089143b3f0155ad6d
shane <shane16@naver.com>
2020-03-11 22:32:20 +0900

    error reseting count each 2 rounds

--- inverse_robotarm/inverse_robotarm.ino
+++ inverse_robotarm/inverse_robotarm.ino
@@ -5,9 +5,11 @@ Servo servo1, servo2, servo3, servo4, servo5, servo6;  // create servo object to
 
 float l2=120, l4=121.5;
 float pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, pos5 = 0, pos6 = 0; // variable to store the servo position
-float px=-100,py=-100,pz=100,r1=1,r4=0,r7=0;
+float px,py,pz,r1,r4,r7;
 double Au0=0,Au1=0,Au2=0,Bu0=0,Bu1=0,Bu2=0;
-int count=0;
+static int count=0;
+int times=1;
+int case_number;
 
 struct Solution 
 {
@@ -43,6 +45,66 @@ float calculate(float B0sign,float B1sign)
   Serial.println("");
 }
 
+void movemotor()
+{
+  if(case_number==1)
+  {
+    servo1.write(s1.pos1);            
+    delay(500);
+  
+    servo2.write(s1.pos2);            
+    delay(500);
+  
+    servo3.write(s1.pos3);            
+    delay(500);
+  
+    servo4.write(s1.pos4);            
+    delay(500); 
+  }
+  else if(case_number==2)
+  {
+    servo1.write(s2.pos1);            
+    delay(500);
+  
+    servo2.write(s2.pos2);            
+    delay(500);
+  
+    servo3.write(s2.pos3);            
+    delay(500);
+  
+    servo4.write(s2.pos4);            
+    delay(500);      
+  }
+  else if(case_number==3)
+  {
+    servo1.write(s3.pos1);            
+    delay(500);
+  
+    servo2.write(s3.pos2);            
+    delay(500);
+  
+    servo3.write(s3.pos3);            
+    delay(500);
+  
+    servo4.write(s3.pos4);            
+    delay(500); 
+  }
+  else
+  {
+    servo1.write(s4.pos1);            
+    delay(500);
+  
+    servo2.write(s4.pos2);            
+    delay(500);
+  
+    servo3.write(s4.pos3);            
+    delay(500);
+  
+    servo4.write(s4.pos4);            
+    delay(500); 
+  }
+}
+
 void setup() 
 {
   Serial.begin(9600);
@@ -62,13 +124,15 @@ void setup()
 
 void loop() 
 { 
+  Serial.print("round");Serial.print(times);Serial.print(": ");
+  Serial.println(count);
   if(count==0)
   {  
-  Serial.println("Enter px py pz r1 r4 r7:");
-  count=1;
+    Serial.println("Enter px py pz r1 r4 r7:");
+    count=1;
   }
   
-  if(Serial.available())
+  else if(count==1&&Serial.available())
   {
     px=Serial.parseFloat();
     py=Serial.parseFloat();
@@ -80,11 +144,16 @@ void loop()
     Serial.println("Entered values:");
     Serial.print(px);Serial.print(" ");Serial.print(py );Serial.print(" ");Serial.println(pz);
     Serial.print(r1 );Serial.print(" ");Serial.print(r4 );Serial.print(" ");Serial.println(r7);Serial.println("");
+    count=2;
+  }
 
+  else if(count==2)
+  {
     if(pow(px,2)+pow(py,2)+pow(pz,2)>pow((l2+l4),2))//longer than robot arm
     {
       Serial.println("not reachable point");
       Serial.println("Re-enter:");
+      count=0;
     }
     else
     {
@@ -94,72 +163,48 @@ void loop()
       s1.pos2=pos2;
       s1.pos3=pos3;
       s2.pos4=pos4;
-
+  
       Serial.println("case2:");
       calculate(1,-1);
       s2.pos1=pos1;
       s2.pos2=pos2;
       s2.pos3=pos3;
       s2.pos4=pos4;
-
+  
       Serial.println("case3:");
       calculate(-1,1);
       s3.pos1=pos1;
       s3.pos2=pos2;
       s3.pos3=pos3;
       s3.pos4=pos4;
-
+  
       Serial.println("case4:");
       calculate(-1,-1);
       s4.pos1=pos1;
       s4.pos2=pos2;
       s4.pos3=pos3;
       s4.pos4=pos4;
-
-      
-      servo1.write(pos1);            
-      delay(500);
-    
-      servo2.write(pos2/2);            
-      delay(500);
-      servo2.write(pos2);            
-      delay(500);
-    
-      servo3.write(pos3);            
-      delay(500);
-    
-      servo4.write(pos4);            
-      delay(500);
-  
-       if(count==1)
-    {
-      Serial.println("enter pos5");
-      if(Serial.available())
-      {
-        pos5=Serial.parseFloat();
-        Serial.print("pos5: ");
-        Serial.println(pos5);
-        servo5.write(pos5); 
-  /*
-        Serial.println("enter pos6");
-        pos6=Serial.parseFloat();
-        Serial.print("pos6: ");
-        Serial.println(pos6);
-        servo6.write(pos6); 
-        */
-        count=0;
-      }
-     }
-    /*
-      servo5.write(pos5);            
-      delay(15);
-    
-      Serial.print("6: ");
-      scanf("%d", &pos6);
-      
-      servo6.write(pos6);            
-      delay(15);
-      */
+      count=3;
     }
-   }
-}
+  }//closing count2
+
+  else if(count==3)
+  {
+    //ask which case
+    Serial.println("Which case do you like?: 1,2,3,4");
+    if(Serial.available())
+    {
+      case_number=Serial.parseInt();
+      Serial.print("You chose case");Serial.println(case_number);
+      count=4;
+    }
+  }//closing count3
+
+  else if(count=4)
+  {
+    movemotor();
+    count=0;
+  }
+  else count=1;
+ times++; 
+}//closing loop
